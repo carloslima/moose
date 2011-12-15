carloslima/moose

commit 6f496445489688cc76b0de61d66ab515f1c608b8

allow overriding class and role types with equivalent types

lib/Moose/Util/TypeConstraints.pm
@@ -158,11 +158,13 @@ sub create_class_type_constraint {
     my $pkg_defined_in = scalar( caller(1) );
 
     if (my $type = $REGISTRY->get_type_constraint($class)) {
-        _confess(
-            "The type constraint '$class' has already been created in "
-          . $type->_package_defined_in
-          . " and cannot be created again in "
-          . $pkg_defined_in )
+        if (!($type->isa('Moose::Meta::TypeConstraint::Class') && $type->class eq $class)) {
+            _confess(
+                "The type constraint '$class' has already been created in "
+              . $type->_package_defined_in
+              . " and cannot be created again in "
+              . $pkg_defined_in )
+        }
     }
 
     my %options = (
@@ -189,11 +191,13 @@ sub create_role_type_constraint {
     my $pkg_defined_in = scalar( caller(1) );
 
     if (my $type = $REGISTRY->get_type_constraint($role)) {
-        _confess(
-            "The type constraint '$role' has already been created in "
-          . $type->_package_defined_in
-          . " and cannot be created again in "
-          . $pkg_defined_in )
+        if (!($type->isa('Moose::Meta::TypeConstraint::Role') && $type->role eq $role)) {
+            _confess(
+                "The type constraint '$role' has already been created in "
+              . $type->_package_defined_in
+              . " and cannot be created again in "
+              . $pkg_defined_in )
+        }
     }
 
     my %options = (

commit 37329d2a5c2dc821dc490f315aabeaaf6fe98bdc

Fix bug with multiple class_type statements.
We would previously override the existing class_type with a new one, thereby losing any coercions that
were defined on the previous class_type.

This bug was introduced in commit 6f49644548

lib/Moose/Util/TypeConstraints.pm
@@ -165,6 +165,9 @@ sub create_class_type_constraint {
               . " and cannot be created again in "
               . $pkg_defined_in )
         }
+        else {
+            return $type;
+        }
     }
 
     my %options = (

t/type_constraints/class_type_constraint.t
@@ -92,4 +92,25 @@ ok( $type->is_subtype_of(Moose::Meta::TypeConstraint::Class->new( name => "__ANO
     ok($child->is_subtype_of($parent));
 }
 
+{
+    my $type;
+    is( exception { $type = class_type 'MyExampleClass' }, undef, 'Make initial class_type' );
+    coerce 'MyExampleClass', from 'Str', via { bless {}, 'MyExampleClass' };
+    # We test class_type keeping the existing type (not making a new one) here.
+    is( exception { is(class_type('MyExampleClass'), $type, 're-running class_type gives same type') }, undef, 'No exception making duplicate class_type' );;
+
+    # Next define a class which needs this type and it's original coercion
+    # Note this has to be after the 2nd class_type call to test the bug as M::M::Attribute grabs
+    # the type constraint which is there at the time the attribute decleration runs.
+    {
+        package HoldsExample;
+        use Moose;
+
+        has foo => ( isa => 'MyExampleClass', is => 'ro', coerce => 1, required => 1 );
+        no Moose;
+    }
+
+    is( exception { isa_ok(HoldsExample->new(foo => "bar")->foo, 'MyExampleClass') }, undef, 'class_type coercion works' );
+}
+
 done_testing;